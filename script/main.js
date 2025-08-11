d9f6beb5bb54d572a10001b555147233b8263aed
Nikhildath <105769771+Nikhildath@users.noreply.github.com>
2025-08-11 18:13:00 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -51,7 +51,7 @@ function updateUI(data) {
 
   // Water Level
   const waterLevelStatus = getStatus(data.water_level, [20, 40, 60], ["Low", "Average", "Good", "High"]);
-  document.getElementById("waterLevel").innerText = `${waterLevelStatus.text} (${data.water_level} cm)`;
+  document.getElementById("waterLevel").innerText = `${waterLevelStatus.text} (${data.water_level} %)`;
   updateGauge("waterlevel-bar", ".circular-chart.waterlevel .gauge-value", data.water_level, 100, "%", waterLevelStatus.text);
 
   // pH Level
@@ -364,4 +364,5 @@ document.getElementById('fab').onclick = () => {
   document.getElementById('fab').style.transform = 'rotate(360deg)';
   setTimeout(() => document.getElementById('fab').style.transform = '', 400);
 };
+
 
